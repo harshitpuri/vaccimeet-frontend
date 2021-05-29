1fbc1db7b936d5ea2c8e1eaa8b9ac67dd80b7f6e
harshitpuri28 <harshit.puri28@gmail.com>
2021-05-30 05:09:09 +0530

    added config for API_URL and showcase from home page

---
+++ components/Showcase.js
@@ -0,0 +1,12 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+    return (
+        <div>
+            <div className={styles.showcase}>
+                <h1>Enter H1 here</h1>
+                <h2>Enter h2 here</h2>
+            </div>
+        </div>
+    )
+}
