1e280d35d462627b65674567ea69629cc988a84f
harshitpuri28 <harshit.puri28@gmail.com>
2021-05-30 13:15:47 +0530

    MeetItem module and temp API routes added to index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import MeetItem from '@/components/MeetItem'
+import { API_URL } from '@/config/index'
 
-export default function MeetsHome() {
+export default function MeetsHome({ meets }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Meets</h1>
+      {meets.length === 0 && <h3>No Meets to show</h3>}
+
+      {meets.map((evt) => (
+        <MeetItem key={evt.id} evt={evt} />
+      ))}
+
+      {meets.length > 0 && (
+        <Link href='/meets'>
+          <a className='btn-secondary'>View All Meets</a>
+        </Link>
+      )}
     </Layout>
   )
 }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/meets`)
+  const meets = await res.json()
+
+  return {
+    props: { meets:meets.slice(0, 3) },
+    revalidate: 1
+  }
+
+} 
