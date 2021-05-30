54c3b1be063baee510e672dff438b575c963e5bb
harshitpuri28 <harshit.puri28@gmail.com>
2021-05-30 08:15:49 +0530

    showcase added to layout, router added to show it only on homepage

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
+import {useRouter} from 'next/router'
 import Header from './Header'
 import Footer from './Footer'
+import Showcase from './Showcase'
 import styles from '@/styles/Layout.module.css'
 
 export default function Layout({ title, keywords, description, children }) {
+    
+    const router = useRouter()
+    
     return (
 
         <div>
@@ -15,11 +20,13 @@ export default function Layout({ title, keywords, description, children }) {
 
         <Header />
 
+        {router.pathname === '/' && <Showcase />}
+
         <div className={styles.container}>
             {children}
         </div>
 
-        
+        <Footer />
         </div>
     )
 }
